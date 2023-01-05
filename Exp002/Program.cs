87b5f0551814ccd73c0632badb780300d1a2982b
innakornilova <cannes@mail.ru>
2023-01-05 22:12:38 +0300

    Task 2

---
+++ Exp002/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// По двум заданным числам проверять является ли первое квадратом второго
+
+int A = 2;
+int B = 6;
+
+if (A == B * B)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
