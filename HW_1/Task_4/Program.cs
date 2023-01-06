61e85f7dcad08af08f8a37e5f7bd051b679c2d0e
innakornilova <cannes@mail.ru>
2023-01-06 15:41:06 +0300

    Task 4

---
+++ HW_1/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int A = 22;
+int B = 3;
+int C = 9;
+
+int max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+{
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
+
