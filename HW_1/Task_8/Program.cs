3ba54e2c4e93e35a0fad2e121a4985868a977788
innakornilova <cannes@mail.ru>
2023-01-06 16:44:06 +0300

    Task 8

---
+++ HW_1/Task_8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N = 10;
+ArrayLength (1-N);
+index = 0;
+even_numbers;
+
+while (index < ArrayLength)
+{
+    if
+    {
+        number % 2 = 0 
+    number (index) add to even_numbers;
+
+    }
+
+    Index ++   
+
+Console.Write("Четные числа: ");
+Console.WriteLine(even_numbers);
+}
