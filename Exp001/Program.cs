45a6d8748cf58ef49c1db121cf3bdd69c357d7e4
innakornilova <cannes@mail.ru>
2023-01-06 12:54:54 +0300

    HW_1 Task 2

--- Exp001/Program.cs
+++ Exp001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿intA = 15;
+
+intB = 145;
+
+if (intA > intB)
+{
+    Console.WriteLine("max = intA");
+    Concole.WriteLine(max);
+}
+else
+{
+    Console.WriteLine("max = intB");
+    Concole.WriteLine(max);
+}
---
+++ HW_1/Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+intA = 15;
+
+intB = 145;
+
+if (intA > intB)
+{
+    Console.WriteLine("max = intA");
+    Concole.WriteLine(max);
+}
+else
+{
+    Console.WriteLine("max = intB");
+    Concole.WriteLine(max);
+}
+
