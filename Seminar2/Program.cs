c5df915e3ce60c91a4c297c47c189cb2a6e004a7
innakornilova <cannes@mail.ru>
2023-01-06 21:49:07 +0300

    Задача со сбором нового массива

---
+++ Seminar2/Program.cs
@@ -0,0 +1,50 @@
+﻿//  Написать программу, которая на вход получает массив,
+//  а на выходе собирает новый массив только из чисел, кратных 5
+
+// метод создания массива
+// diagram
+// метод заполнения массива
+// diagram
+// метод подсчета чисел кратных 5
+// diagram
+// решение задачи 
+// diagram
+// печать
+
+
+int [] array = {45, 80, 33, 75, 60, 13};
+
+int count = 0;
+int index;
+index = 0;
+
+while(index < array.Length)
+{
+    if (array[index] % 5 == 0)
+    {
+        count++;
+    }
+    index++;
+}
+
+int [] arrayNew = new int[count];
+index = 0;
+int indexNew = 0;
+
+while (index < array.Length)
+{
+   if (array[index] % 5 == 0)
+   {
+        arrayNew[indexNew] = array[index];
+        indexNew++;
+   }
+    index ++;   
+}
+Console.WriteLine(arrayNew);
+index=0;
+while(index <array.Length)
+{
+System.Console.WriteLine(arrayNew[index]);
+index++;
+}
+
