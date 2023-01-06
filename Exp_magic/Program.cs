fdab93fe3f02dec88a9c72df71ac52ecd89c4915
innakornilova <cannes@mail.ru>
2023-01-06 11:52:46 +0300

    Exp magic

--- Exp_magic/Program.cs
+++ Exp_magic/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1, 
+    xb = 1, yb = 30,
+    xc = 40, yc = 30;
+
+Console.SetCursorPosition (xa, ya);
+Console.WriteLine ("+");
+
+Console.SetCursorPosition (xb, yb);
+Console.WriteLine ("+");
+
+Console.SetCursorPosition (xc, yc);
+Console.WriteLine ("+");
+
+
+
