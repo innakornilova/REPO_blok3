24148124202606b7b916b99383d28962fe969f90
innakornilova <cannes@mail.ru>
2023-01-08 17:35:33 +0300

    Task 10

---
+++ HW_2/Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите значение числа A");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int B = A / 10;
+int D = B % 10;
+
+Console.Write("Вторая цифра числа:");
+Console.WriteLine(D);
