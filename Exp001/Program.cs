45615d656e6e48e22a6474487cbdd98d827b588d
innakornilova <cannes@mail.ru>
2023-01-09 20:08:02 +0300

    Exp001

--- Exp001/Program.cs
+++ Exp001/Program.cs
@@ -1,17 +1,1 @@
-﻿Console.WriteLine("Введите значение числа N");
-int N = Convert.ToInt32(Console.ReadLine());
-
-int [] array = {1, 2, 3, 4, 5};
-int N = array.Length;
-int index = 0;
-
-while (index < array.Length)
-{
-    if (array[index] % 2 == 0) 
-    {
-        Console.WriteLine (array[index]);
-    }
-
-    index ++;   
-
-}
+﻿
