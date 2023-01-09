c0ab3b6b88f77565a240202e2d50c9d4870ec0f3
innakornilova <cannes@mail.ru>
2023-01-09 20:04:44 +0300

    Task 13

--- HW_2/Task_13/Program.cs
+++ HW_2/Task_13/Program.cs
@@ -3,15 +3,17 @@
 Console.WriteLine("Введите значение числа А ");
 int A = Convert.ToInt32(Console.ReadLine());
 
-
-int B = A % 100 % 10;
-
 if (A < 100)
 {
     Console.WriteLine ("Третьей цифры нет");
 }
 else 
 {
-    Console.Write ("Третья цифра чила: ");
-    Console.WriteLine (B);
+    if (A < 1000) 
+    {
+       int B = A % 100;    
+    }
+   
+Console.Write ("Третья цифра чила: ");
+Console.WriteLine (B);
 }       
