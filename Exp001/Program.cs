6b058dfc0f8b90ab47151585f0aaf355fac6410b
innakornilova <cannes@mail.ru>
2023-01-09 15:19:25 +0300

    Task 8 changes

--- Exp001/Program.cs
+++ Exp001/Program.cs
@@ -1,2 +1,17 @@
-﻿int A=1001;
-A = Convert.ToString();      
+﻿Console.WriteLine("Введите значение числа N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] array = {1, 2, 3, 4, 5};
+int N = array.Length;
+int index = 0;
+
+while (index < array.Length)
+{
+    if (array[index] % 2 == 0) 
+    {
+        Console.WriteLine (array[index]);
+    }
+
+    index ++;   
+
+}
--- HW_1/Task_8/Program.cs
+++ HW_1/Task_8/Program.cs
@@ -1,8 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+Console.WriteLine("Введите значение числа N");
+int N = Convert.ToInt32(Console.ReadLine());
+
 int [] array = {1, 2, 3, 4, 5};
-int N = array.Length;
-int index = 0;
+int index;
+index = 0;
 
 while (index < N)
 {
