5488de1494d8dbe5b4c5fbf7f6df8c3ceb6bf565
innakornilova <cannes@mail.ru>
2023-01-10 12:39:21 +0300

    +

--- experiments/Program.cs
+++ experiments/Program.cs
@@ -1,17 +1,1 @@
 ﻿// 
-
-Console.WriteLine("Введите значение числа ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-if (number < 100)
-    Console.WriteLine ("Третьей цифры нет");
-
-else 
-{
-    while (number >= 1000) 
-    {
-       number = number / 10;    
-    }    
-    Console.Write ("Третья цифра числа: ");
-    Console.WriteLine (number % 10);
-}
