e951e3d6526084c6a5a15035927f6c6bc7e11a5c
innakornilova <cannes@mail.ru>
2023-01-11 12:43:49 +0300

    Task 19+ drawio

--- HW_3/Task19/Program.cs
+++ HW_3/Task19/Program.cs
@@ -4,4 +4,21 @@
 // 23432 -> да
 // НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ
 
+Console.WriteLine("Введите значение 5ти значного числа ");
+int fiveDigitNum = Convert.ToInt32(Console.ReadLine());
+
+palindrome(fiveDigitNum);
+
+void palindrome(int fiveDigitNum)
+{
+    if (fiveDigitNum % 10 == fiveDigitNum / 10000)
+    {
+        if (fiveDigitNum / 10 % 10 == fiveDigitNum / 1000 % 10)
+        {
+            Console.WriteLine("Это число палиндром");
+        }
+        else Console.WriteLine("Это число не является палиндромом");
+    }
+    else Console.WriteLine("Это число не является палиндромом");
+}
 
