b1aeba8393dce8e2990013949b654a7f20f1cbf2
innakornilova <cannes@mail.ru>
2023-01-11 11:57:00 +0300

    Task 23 & drawio

---
+++ HW_3/Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int cubing(int number, int[] arr, int index)
+{
+    while (index < number)
+    {
+        Console.WriteLine(Math.Pow(arr[index], 3));
+        index++;
+    }
+
+    return index;
+}
+
+Console.WriteLine("Введите значение числа N ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица кубов чисел от 1 до ");
+Console.WriteLine(number);
+
+int[] arr = Enumerable.Range(1, number).ToArray();
+int index;
+index = 0;
+
+index = cubing(number, arr, index);
+
+
+
