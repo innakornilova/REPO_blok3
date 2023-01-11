1b6f63a33e68ac056c7093cd4c3b1a0ab671f483
innakornilova <cannes@mail.ru>
2023-01-11 11:01:49 +0300

    Task 15 & 10 small changes

--- HW_2/Task10/Program.cs
+++ HW_2/Task10/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 Console.WriteLine("Введите значение числа A");
-int A = Convert.ToInt32(Console.ReadLine());
+int threeDigitNum = Convert.ToInt32(Console.ReadLine());
 
-int D = A / 10 % 10;
+int secondDigit = threeDigitNum / 10 % 10;
 
 Console.Write("Вторая цифра числа:");
-Console.WriteLine(D);
+Console.WriteLine(secondDigit);
--- HW_2/Task_15/Program.cs
+++ HW_2/Task_15/Program.cs
@@ -7,11 +7,11 @@ int day = Convert.ToInt32(Console.ReadLine());
 string GetInfoByDayOfWeek (int day)
 {
     if (day < 6)
-        return ("Рабочий день"); 
+        return "Рабочий день"; 
     else if (day > 5 && day < 8)
-        return ("Выходной день");     
+        return "Выходной день";     
     else 
-        return ("Введите другое число"); 
+        return "Введите другое число"; 
 }
 
 Console.WriteLine(GetInfoByDayOfWeek(day));
