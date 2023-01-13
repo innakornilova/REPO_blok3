392fa56a168b32eb4386afcda56319ede11d902b
innakornilova <cannes@mail.ru>
2023-01-13 22:20:48 +0300

    new methods

--- experiments/Library.cs
+++ experiments/Library.cs
@@ -1,31 +1,75 @@
-// если берем Метод из библиотеки, то перед названием Метода пишем : public static 
+// если используем Метод из библиотеки, то перед названием Метода пишем : public static (см. HW4\Task777)
 
-// Метод создания массива n1
+// //Метод запроса числа у пользователя
+// int GetNumberByUser(string text)
+// {
+//     Console.WriteLine("Введите значение числа N ");
+//     double number = Convert.ToInt32(Console.ReadLine());
+// }
+
+// //Метод генерации случайного размера массива
+// int[] CreateArr()
+// {
+//     int createArr = new int[Random.Shared.Next(10,20)];
+//     return createArr;
+// }
+
+// // Метод ввода N
+//  int GetNumberByUser(string text)
+// {
+//     Console.Write(text + ": ");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// //Метод создания массива n*1
 // int [] CreateArray(int size)
 // {
 //     return new int[size];
 // }
 
-// Метод создания массива n2
+// //Метод создания массива n*2
 // //     int [] <- (int [])
 // int [] CreateArr(int number)
 // {
-//      int[] arr = new int[number]//     
+//      int[] arr = new int[number]     
 //      return arr;
 // }
 
-// Метод заполнения массива 0 и 1
-// //    void <- (int[])
-    // void GetBinArray(int[] colNumber)
+// //Метод заполнения массива рандомными числами (например, от 0 до 9)
+    // void FillArray(int[] colNumber)
     // {
     //     int count = colNumber.Length; 
-    //     colNumber[0] = 1;        // left number != 0              
+                
+    //     for (int i = 0; i < count; i++)   
+    //     {
+    //         colNumber[i] = Random.Shared.Next(1, 10); 
+    //     } 
+    // }
 
-    //     for (int i = 1; i < count; i++)   
+// //Метод заполнения массива 0 и 1
+// //    void <- (int[])
+    // void FillBinArray(int[] colNumber)
+    // {
+    //     int count = colNumber.Length; 
+    // //   colNumber[0] = 1;        // if left number != 0              
+
+    //     for (int i = 0; i < count; i++)   
     //     {
     //         colNumber[i] = Random.Shared.Next(2); // Random.Shared.Next(2) - т.е. берет числа <2
+    //// ex.int[] arr = Enumerable.Range(1, N).ToArray();
     //     }
     // }
+
+// Метод копирования массива
+// void ClonArray(int[] filledArray, int[] x)
+// {
+//     int count = filledArray.Length; 
+                
+//     for (int i = 0; i < count; i++)   
+//     {
+//     x[i] = filledArray[i]; 
+//     } 
+// }
 
 // // Печать массива
 // // string <- (int[])
@@ -67,30 +111,14 @@
 //     return result;
 //     }
 
-// // Конвертация из 2го в 10е
-//     public static int GetDecArray(int[] binNumber)
-//     { 
-//         int count = binNumber.Length;
-//         int exponent = binNumber.Length-1;
-//         int result = 0;
-        
-//         for (int m = 0; m < binNumber.Length; m++)   
-//         {
-//             if (binNumber[m] == 1)
-//             {
-//                 result += Convert.ToInt32(Math.Pow(2, exponent)); // result = result +
-//             } 
-//             exponent --;
-//         }
-//     return result;
-//     }
 
-// Метод перевода числа из 10 в 16 систему
-// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-//                      A  B  C  D  E  F 
+// //Метод перевода числа из 10 в 16 систему
+// //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+//  //                    A  B  C  D  E  F 
 
-// int val = 301;
+// int val = 301231;
 // // 16 система
+// string res = "";
 // while (val != 0)
 // {
 //     int b = val % 16;
@@ -99,6 +127,7 @@
 //     else if (b == 12) res = $"C{res}";
 //     else if (b == 13) res = $"D{res}";
 //     else if (b == 14) res = $"E{res}";
+//     else if (b == 15) res = $"F{res}";
 //     else res = $"{b}{res}";
 //     val = val / 16;
 // }
--- experiments/Program.cs
+++ experiments/Program.cs
@@ -1,2 +1,18 @@
-﻿// 
+﻿//
 
+    int[] Mult(int[] array)
+    {   
+        int[] array = {1, 3, 4, 5, 6};
+        int[] result = new int [array.Length/2]; 
+        int count = array.Length;
+                
+        for (int i = 0; i < count / 2; i++)   
+        {
+            result[i] = array[i] * array[count - 1 - i];
+        } 
+                
+    return result;
+    }
+
+    
+    Console.WriteLine(result);
