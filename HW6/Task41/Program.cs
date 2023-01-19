64ba41bc620a0cc40cf5e6c9a03934a22807a6f4
innakornilova <cannes@mail.ru>
2023-01-19 20:54:19 +0300

    HW6 Task 41, 43

--- HW6/Task41/Program.cs
+++ HW6/Task41/Program.cs
@@ -4,4 +4,33 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+string[] data = Console.ReadLine().Split(' ');
+for (int i = 0; i < data.Length; i++)
+{
+    Console.Write(" " + int.Parse(data[i]) + ",");
+}
+Console.WriteLine();
+
+
+int[] Result(int[] origArray)
+{
+    int count = 0;
+    
+    for (int i = 0; i < origArray.Length; i++)
+    {
+        if (origArray[i] > 0) count++;
+    }
+}
+
+string data;
+int[] count = Result(data);
+Console.WriteLine(count);
+
+
+// Console.Write("Введите числа через запятую: ");
+// string words = Console.ReadLine();
+// origArray = words.Split(",");
+// int[] arr = Count(origArray);
+// Console.WriteLine(arr);
+
 
