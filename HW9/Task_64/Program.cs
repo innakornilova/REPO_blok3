0a0495027a1d19e837e577d3ab27a3e86ac77017
innakornilova <cannes@mail.ru>
2023-01-29 16:09:47 +0300

    Task 64

---
+++ HW9/Task_64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int min = 1;
+int num = N;
+void ColNum(int num)
+{
+        if(num % num == 0)
+        {
+           Console.Write(num + " ");
+        }
+        num--;
+    if(num < min) 
+    return;
+    ColNum(num);
+}
+ColNum(num);
+
